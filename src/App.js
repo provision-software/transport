cbdb6fba1a4aa894e46f48dfdfe6c1a2a41bd791
Ryan Hoffman <rhoffman@Airmac.local>
2020-06-20 23:08:34 -0400

    add top navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import SignIn from './components/SignIn';
+import TopNavBar from './components/TopNavBar';
 
 function App() {
   return (
     <div className="App">
+      <TopNavBar />
       <SignIn />
     </div>
   );
