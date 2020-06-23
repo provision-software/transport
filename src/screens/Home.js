a9fce6869fd753187b55d4b3f2c7a194e4123d6a
Ryan Hoffman <rhoffman@Airmac.local>
2020-06-22 22:29:52 -0400

    update home with navbar

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TopNavBar from '../components/TopNavBar';
+
+
+class home extends React.Component {
+    render() {
+        return(
+            <TopNavBar />
+        );
+    }
+}
